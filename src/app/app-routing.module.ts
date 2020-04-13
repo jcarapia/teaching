ce2e4076c058b25e5b51eabab19e90805ec12590
Jorge Carapia <jorge.carapia@candid.org>
2020-04-12 22:21:53 -0700

    added section components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { SectionOneComponent } from './section-one/section-one.component';
+import { SectionTwoComponent } from './section-two/section-two.component';
+import { SectionThreeComponent } from './section-three/section-three.component';
+import { SectionFourComponent } from './section-four/section-four.component';
+import { SectionFiveComponent } from './section-five/section-five.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'sectionOne', component: SectionOneComponent  },
+  { path: 'sectionTwo', component: SectionTwoComponent  },
+  { path: 'sectionThree', component: SectionThreeComponent  },
+  { path: 'sectionFour', component: SectionFourComponent  },
+  { path: 'sectionFive', component: SectionFiveComponent  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { SectionOneComponent } from './section-one/section-one.component';
+import { SectionTwoComponent } from './section-two/section-two.component';
+import { SectionThreeComponent } from './section-three/section-three.component';
+import { SectionFourComponent } from './section-four/section-four.component';
+import { SectionFiveComponent } from './section-five/section-five.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    SectionOneComponent,
+    SectionTwoComponent,
+    SectionThreeComponent,
+    SectionFourComponent,
+    SectionFiveComponent
   ],
   imports: [
     BrowserModule,
